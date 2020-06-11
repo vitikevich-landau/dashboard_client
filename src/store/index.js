ce05e9ca6dbea7dee401ebcef41608d778e59f1a
vitikevich <vitikevich@gmail.com>
2020-06-11 17:29:36 +0700

    # dev preparing data

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,14 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { DATA_URL } from "@/configs";
 // eslint-disable-next-line no-unused-vars
-import { fetchData, usedSheetNames, gt, getRows, getRowValue, toWorkBookMap, toRecords, getYears } from "@/utils/dataSet";
+import { DATA_URL, MONTHS } from "@/configs";
+import {
+  fetchData,
+  toWorkBookMap,
+  toRecords,
+  // eslint-disable-next-line no-unused-vars
+  getYears, toRound, getDistricts, getInstitutions, mergeWithMonths
+} from "@/utils/dataSet";
 // eslint-disable-next-line no-unused-vars
 import { mergeRecords, Records } from "@/models/Records";
 
@@ -58,12 +64,90 @@ export default new Vuex.Store({
 
       const records = toRecords(data);
       const years = getYears(records).sort((a, b) => a - b);
+      const districts = getDistricts(records);
+      const institutions = getInstitutions(records);
 
-      console.log(years);
+      // console.log(years);
 
       commit('setAccountingSections', sheetNames);
       commit('setRecords', records);
       commit('setYears', years);
+      commit('setDistricts', districts);
+      commit('setInstitutions', institutions);
+
+      /*
+      *   APPLYING FILTERS
+      * */
+      // const selectedYears = [2020, 2019, 2017, 2000, 2002];
+      const selectedYears = [2018];
+      const selectedAccSections = ["Участки", "Здания"];
+      const selectedAccDistricts = ["Горно-Алтайск", "Улаганский", "Усть-Канский", "Усть-Коксинский"];
+      const selectedInstitutions = ["ДПИ2", "ДПИ3", "ДПИ4", "УСПН_Город", "УСПН_Кокса", "УСПН_Кош_Агач", "УСПН_Майма", "УСПН_Онгудай"];
+
+      const filtered = _(records)
+        .filter(rec => selectedYears.includes(rec.date.getFullYear()))
+        .filter(rec => selectedAccSections.includes(rec.accountingSection))
+        .filter(rec => selectedAccDistricts.includes(rec.district))
+        .filter(rec => selectedInstitutions.includes(rec.institution))
+        .value();
+
+
+      /*
+      *   GROUP
+      *   group by years -> accountSection
+      * */
+
+      const byYear = _.groupBy(filtered, r => r.year);
+      const byMonths = _(byYear)
+        .mapValues(row =>
+          mergeWithMonths(
+            _.groupBy(row, rec => rec.month)
+          )
+        )
+        .value();
+
+      /*
+      *   Break Point here prepared data
+      *
+      * */
+
+      console.log(
+        _(_.values(byMonths)[0])
+          .map(row => {
+              if(row) {
+                return row.reduce((acc, rec) => toRound(acc + rec.amount), 0)
+              } else {
+                return undefined
+              }
+            }
+          )
+          .value()
+      );
+
+
+      /*
+      *   GROUPINGS
+      * */
+      // console.log(
+      //   _(records)
+      //     .groupBy(rec => rec.accountingSection)
+      //     // .mapValues(
+      //     //   row => row.reduce(
+      //     //     (acc, rec) => toRound(acc + rec.amount), 0)
+      //     .mapValues(section =>
+      //       _(section)
+      //         .groupBy(rec => rec.date.getFullYear())
+      //         .mapValues(rec =>
+      //           _.groupBy(rec, r => r.date.getMonth())
+      //         )
+      //         /*
+      //         *   pad missing months values with undefined
+      //         * */
+      //         .mapValues(rec => _.merge(_.zipObject(_.keys(MONTHS)), rec))
+      //         .value()
+      //     )
+      //     .value()
+      // );
 
 
       // console.log(
