58b2185ce91db3c7394a57e6ce30b35686dcbc58
vitikevich <vitikevich@gmail.com>
2020-06-13 17:56:42 +0700

    # modify Records

--- src/models/Records.js
+++ src/models/Records.js
@@ -1,27 +1,60 @@
-import {getRows, getRowValue, usedSheetNames} from "@/utils/dataSet";
-// import { Record } from "@/models/Record";
-// import { MONTHS } from "@/configs";
+import { toRecords } from "@/utils/dataSet";
 import _ from 'lodash';
+import { Record } from "@/models/Record";
 
 export class Records {
+  #workBook;
+  #raw;
+  #records;
+
   constructor(workBook) {
-    this.records = this._toWorkBookMap(workBook);
+    this.#workBook = workBook;
+    this.#raw = toRecords(this.#workBook);
+    this.#records = this.#raw
+      .map(r => new Record(r[0], r[1], r[2], r[3], r[4], r[5]));
   }
 
-  _toWorkBookMap(workBook) {
-    const sheetNames = usedSheetNames(workBook);
-    const sheets = sheetNames.map(v => workBook.Sheets[v]);
-    const map = sheets
-      .map(r => getRows(r, getRowValue))
-      .map((r, i) => r
-        .map(
-          s => [...s, sheetNames[i]]
-        )
-      );
-    return _.zipObject(sheetNames, map);
+  get years() {
+    return _.uniq(this.#records.map(r => r.year)
+      .sort((a, b) => a - b));
+  }
+
+  get accounts() {
+    return _.uniq(this.#records.map(r => r.account));
+  }
+
+  get districts() {
+    return _.uniq(this.#records.map(r => r.district));
+  }
+
+  get institutions() {
+    return _.uniq(this.#records.map(r => r.institution));
+  }
+
+  get serviceTypes() {
+    return _.uniq(this.#records.map(r => r.serviceType));
+  }
+
+  groupBy(fields) {
+    let i = 0, len = fields.length - 1;
+
+    const recursive = (rcs, i) =>
+      i >= len
+        ? _.groupBy(rcs, r => r[fields[i]])
+        : _
+          .mapValues(
+            _.groupBy(rcs, r => r[fields[i]]),
+            r => recursive(r, i + 1)
+          );
+
+    return recursive(this.#records, i);
+  }
+
+  filter(callbackfn, thisArg) {
+    return this.#records.filter(callbackfn, thisArg);
   }
 
   count() {
-    return _.values(this.records).length;
+    return _.values(this.#records).length;
   }
 }
