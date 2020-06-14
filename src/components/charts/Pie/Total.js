b4cc6c129c78ff118ecdc188fe1d68db64c29f15
Viktor <vitikevich@gmail.com>
2020-06-14 10:28:25 +0700

    # add Pie, total diagram

---
+++ src/components/charts/Pie/Total.js
@@ -0,0 +1,32 @@
+import {Doughnut, mixins} from "vue-chartjs";
+// import ChartDataLabels from 'chartjs-plugin-datalabels';
+
+const {reactiveProp} = mixins;
+
+export default {
+  extends: Doughnut,
+  mixins: [reactiveProp],
+  // plugins: [ChartDataLabels],
+  data() {
+    return {
+      options: {
+        title: {
+          display: true,
+          text: 'Общий расход'
+        },
+        tooltips: {
+          mode: 'dataset',
+          intersect: true
+        },
+        animation: {
+          duration: 450
+        }
+      }
+    }
+  },
+  mounted() {
+    // this.chartData is created in the mixin.
+    // If you want to pass options please create a local options object
+    this.renderChart(this.chartData, this.options);
+  }
+};
