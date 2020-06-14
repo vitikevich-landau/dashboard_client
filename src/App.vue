a0cacfef2b83fcac6f1f586907a9a39b111a8b9c
Viktor <vitikevich@gmail.com>
2020-06-14 21:04:57 +0700

    # add vue-multiselect, sandbox

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,159 @@
 <template>
   <div id="app">
-    <!-- Контент разбит на 2 row, т.к. содержимое первого row логически не связано с контентом второго row -->
-    <div class="row" v-if="dataIsLoaded">
-      <div class="col-2">
-        <label>
-          <select class="custom-select" v-model="selectedYears">
-            <option
-                v-for="year in years" :key="year">{{year}}
-            </option>
-          </select>
-        </label>
+    <div class="container-fluid" v-if="dataIsLoaded">
+
+      <!--  Filters -->
+      <div class="row col-8">
+        <div class="col-md offset-sm-2">
+          <label for="year">Год</label>
+          <!--          <Multiselect-->
+          <!--              required="true"-->
+          <!--              id="year"-->
+          <!--              :options="VMGroupYear"-->
+          <!--              :selected="lastYear"-->
+          <!--              select-label="Выбрать"-->
+          <!--              selected-label="Выбрано"-->
+          <!--              deselectLabel="Удалить"-->
+          <!--              :closeOnSelect="false"-->
+          <!--              :multiple="true"-->
+          <!--              :limit="2"-->
+          <!--              :limitText="count => `и ещё ${count}`"-->
+          <!--              :searchable="false"-->
+          <!--              :group-select="true"-->
+          <!--              selectGroupLabel=""-->
+          <!--              deselectGroupLabel=""-->
+          <!--              group-label="category"-->
+          <!--              group-values="years"-->
+          <!--              placeholder=""-->
+          <!--              v-model="selectedYears"-->
+          <!--          />-->
+
+          <!-- Single option for test -->
+          <Multiselect
+              required="true"
+              id="year"
+              :options="years"
+              :selected="lastYear"
+              select-label="Выбрать"
+              selected-label="Выбрано"
+              deselectLabel="Удалить"
+              :searchable="false"
+              placeholder=""
+              v-model="selectedYears"
+          />
+          <!-- Single option for test -->
+
+        </div>
+        <div class="col-sm">
+          <label for="institutions">Учреждение</label>
+          <Multiselect
+              required="true"
+              id="institutions"
+              :options="VMGroupInstitutions"
+              select-label="Выбрать"
+              selected-label="Выбрано"
+              deselectLabel="Удалить"
+              :closeOnSelect="false"
+              :multiple="true"
+              :limit="1"
+              :limitText="count => `и ещё ${count}`"
+              :searchable="false"
+              :group-select="true"
+              selectGroupLabel=""
+              deselectGroupLabel=""
+              group-label="category"
+              group-values="institutions"
+              placeholder=""
+              v-model="selectedInstitutions"
+          />
+        </div>
+        <div class="col-sm">
+          <label for="districts">Район</label>
+          <Multiselect
+              required="true"
+              id="districts"
+              :options="VMGroupDistricts"
+              select-label="Выбрать"
+              selected-label="Выбрано"
+              deselectLabel="Удалить"
+              :closeOnSelect="false"
+              :multiple="true"
+              :limit="1"
+              :limitText="count => `и ещё ${count}`"
+              :searchable="false"
+              :group-select="true"
+              selectGroupLabel=""
+              deselectGroupLabel=""
+              group-label="category"
+              group-values="districts"
+              placeholder=""
+              v-model="selectedDistricts"
+          />
+        </div>
       </div>
-      <div class="col-lg-12">
-        <BarChart
-            :chart-data="chartData"
-            :options="chartOptions"
-            :height="160"
-        />
-        <h3>{{recordsCount}} - {{ selectedYears }}</h3>
+
+      <!--  Filters -->
+
+      <div class="row chart-section">
+        <div class="col-lg-9">
+          <BarChart
+              :chart-data="chartData"
+          />
+        </div>
+        <!--<div class="col-lg-4">
+          <BarChart
+              :chart-data="chartData"
+          />
+        </div>-->
+        <!--  <div class="col-lg-3">
+            <BarChart
+                :chart-data="chartData"
+            />
+          </div>-->
+        <div class="col-lg-3">
+          <PieChart
+              :chart-data="pieChartData"
+          />
+        </div>
       </div>
+
+      <h3>{{({[selectedYears] : filtered.count()})}}</h3>
     </div>
+    <!-- end -->
     <br>
   </div>
 </template>
 
 <script>
 
-  // import HelloWorld from '@/charts/HelloWorld.vue'
   import BarChart from "@/components/charts/Bar/Bar";
+  import PieChart from "@/components/charts/Pie/Total";
   import store from '@/store';
-  import { mapActions, mapGetters } from 'vuex';
+  import {mapActions, mapGetters} from 'vuex';
   // eslint-disable-next-line no-unused-vars
-  import { COLORS, MONTHS } from "@/configs";
-  import chartOptions from '@/components/charts/Bar/options';
+  import {COLORS, MONTHS} from "@/configs";
   import _ from 'lodash';
-  import { mergeWithMonths, toRound } from "@/utils/dataSet";
+  import {mergeWithMonths, toRound} from "@/utils/dataSet";
+  import Multiselect from 'vue-multiselect';
 
   export default {
     name: 'App',
     store,
     components: {
-      BarChart
+      BarChart,
+      PieChart,
+      // eslint-disable-next-line vue/no-unused-components
+      Multiselect
     },
     data() {
       return {
         title: "Dashboard App",
-        chartOptions,
-        selectedYears: null
+        /*
+        *   Filters
+        * */
+        selectedYears: [],
+        selectedInstitutions: [],
+        selectedDistricts: [],
       };
     },
     computed: {
@@ -58,18 +165,67 @@
         'years',
         'lastYear',
         'districts',
+        'institutions'
       ]),
-      filtered() {
-        return this.records
-          .filter(r => r.year === +this.selectedYears)
+
+      colors: () => _.values(COLORS),
+
+      /*
+      *   VM - vue Multiselect
+      * */
+      // VMGroupYear() { return [{category: 'Все', years: this.years}]; },
+      VMGroupInstitutions() { return [{category: 'Все', institutions: this.institutions}]; },
+      VMGroupDistricts() { return [{category: 'Все', districts: this.districts}]; },
+
+      /*
+      *   Total chart
+      * */
+      pieChartData() {
+        const groupByAccount = this.filtered.groupBy(['account']);
+        const reducedTotalAmount = _.values(groupByAccount)
+          .map(row => _.reduce(row, (acc, rec) =>
+              toRound(acc + rec.amount), 0
+            )
+          );
+
+        const accounts = _.keys(groupByAccount);
+
+        return {
+          hoverBackgroundColor: COLORS.red,
+          labels: accounts,
+          datasets: [
+            {
+              backgroundColor: accounts.map((_, i) => this.colors[i]),
+              data: reducedTotalAmount
+            }
+          ]
+        };
       },
-      grouped() {
+
+      filtered() {
+        const filtered = this.records
+          .filter(r => +this.selectedYears === r.year)
+          .filter(r => this.selectedDistricts.includes(r.district))
+          .filter(r => this.selectedInstitutions.includes(r.institution));
+
+        console.log(filtered);
+
+        return filtered;
+
+        // return this.records
+        //   // .filter(r => this.selectedYears.includes(r.year))
+        //   /*
+        //   *   For single test
+        //   * */
+        //   .filter(r => +this.selectedYears === r.year);
+      },
+      groupedByAccMonth() {
         return this.filtered.groupBy(['account', 'month']);
       },
       calculated() {
-        const byAccountMonth = this.grouped;
+        const grouped = this.groupedByAccMonth;
 
-        return _(byAccountMonth)
+        const amountByMonths = _(grouped)
           .values()
           .map(account =>
             _.map(mergeWithMonths(account), row =>
@@ -77,19 +233,21 @@
             )
           )
           .value();
+
+        const accounts = _.keys(grouped);
+
+        return {accounts, amountByMonths};
       },
       chartData() {
-        const prepared = this.calculated;
+        const {accounts, amountByMonths} = this.calculated;
 
         const chartData = {
-          labels: _.values(MONTHS)
+          labels: _.values(MONTHS),
         };
 
-        const colors = _.values(COLORS);
-
-        const datasets = _.map(this.accounts, (a, i) =>
-          ({data: prepared[i], label: a, backgroundColor: colors[i]})
-        ) ;
+        const datasets = _.map(accounts, (a, i) =>
+          ({data: amountByMonths[i], label: a, backgroundColor: this.colors[i]})
+        );
 
         return {...chartData, datasets};
       }
@@ -103,12 +261,18 @@
       * */
       await this.$store.dispatch('fetchData');
 
+      // this.selectedYears = [this.lastYear];
+      /*
+      *   For single test
+      * */
       this.selectedYears = this.lastYear;
+      this.selectedDistricts = this.districts;
+      this.selectedInstitutions = this.institutions;
     }
   }
 </script>
 
-<style>
+<style scoped>
   #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
