59d9d3b98cac8180489f0b5e52cc0af23a122207
Viktor <vitikevich@gmail.com>
2020-06-14 08:41:43 +0700

    # Moved options to component

--- src/components/charts/Bar/Bar.js
+++ src/components/charts/Bar/Bar.js
@@ -7,7 +7,44 @@ export default {
   extends: Bar,
   mixins: [reactiveProp],
   // plugins: [ChartDataLabels],
-  props: ["options"],
+  // props: ["options"],
+  data() {
+    return {
+      options: {
+        title: {
+          display: true,
+          text: 'Analytics'
+        },
+        tooltips: {
+          mode: 'index',
+          intersect: true
+        },
+        responsive: true,
+        scales: {
+          xAxes: [{
+            stacked: true,
+          }],
+          yAxes: [{
+            stacked: true,
+            display: true
+          }]
+        },
+        animation: {
+          duration: 450
+        },
+        /*plugins: {
+          datalabels: {
+            color: 'white',
+            textAlign: 'center',
+            font: {
+              weight: "bold",
+              size: 14
+            }
+          }
+        }*/
+      }
+    }
+  },
   mounted() {
     // this.chartData is created in the mixin.
     // If you want to pass options please create a local options object
