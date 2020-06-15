78738baab8e09cba56aad1270cf25cfb78c9a5bb
Viktor <vitikevich@gmail.com>
2020-06-15 21:53:15 +0700

    # create component to filter years

---
+++ src/components/multiselects/Years.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <label for="year">Год</label>
+    <Multiselect
+        id="year"
+        :options="years"
+        :selected="lastYear"
+        select-label="Выбрать"
+        selected-label="Выбрано"
+        deselectLabel="Удалить"
+        :searchable="false"
+        placeholder=""
+        v-model="selectedYears"
+    />
+  </div>
+</template>
+
+<script>
+  import Multiselect from 'vue-multiselect';
+
+  import store from '@/store';
+  import {mapGetters, mapMutations} from 'vuex';
+
+  export default {
+    name: "Districts",
+    store,
+    components: {
+      Multiselect
+    },
+    computed: {
+      ...mapGetters(['years', 'lastYear', 'filterYears']),
+      selectedYears: {
+        get() { return this.filterYears; },
+        set(value) { this.$store.commit('setFilterYears', value); }
+      },
+    },
+    methods: {
+      ...mapMutations(['setFilterYears']),
+    },
+    mounted() {
+
+      this.selectedYears = this.lastYear;
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
