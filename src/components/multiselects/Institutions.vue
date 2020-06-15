310a6b34c624be76ce2c6e8bf0969eaf05bf7d16
Viktor <vitikevich@gmail.com>
2020-06-15 23:57:52 +0700

    # create component to filter institutions

---
+++ src/components/multiselects/Institutions.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <label for="institutions">Учреждения</label>
+    <Multiselect
+        id="institutions"
+        :options="VMGroupInstitutions"
+        select-label="Выбрать"
+        selected-label="Выбрано"
+        deselectLabel="Удалить"
+        :closeOnSelect="false"
+        :multiple="true"
+        :limit="1"
+        :limitText="count => `и ещё ${count}`"
+        :searchable="false"
+        :group-select="true"
+        selectGroupLabel=""
+        deselectGroupLabel=""
+        group-label="category"
+        group-values="institutions"
+        placeholder=""
+        v-model="selectedInstitutions"
+    />
+  </div>
+</template>
+
+<script>
+  import Multiselect from 'vue-multiselect';
+
+  import store from '@/store';
+  import {mapGetters, mapMutations} from 'vuex';
+
+  export default {
+    name: "Districts",
+    store,
+    components: {
+      Multiselect
+    },
+    computed: {
+      ...mapGetters(['institutions', 'filterInstitutions']),
+      selectedInstitutions: {
+        get() { return this.filterInstitutions; },
+        set(value) { this.$store.commit('setFilterInstitutions', value); }
+      },
+      VMGroupInstitutions() { return [{category: 'Все', institutions: this.institutions}]; },
+    },
+    methods: {
+      ...mapMutations(['setFilterInstitutions']),
+    },
+    mounted() {
+
+      this.selectedInstitutions = this.institutions;
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
