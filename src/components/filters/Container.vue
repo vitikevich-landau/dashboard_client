f9fc024c698a7dac95d656a9abd857f2b74cdc76
vitikevich <vitikevich@gmail.com>
2020-06-18 12:58:29 +0700

    # add filters container

---
+++ src/components/filters/Container.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="row col-6 offset-1">
+    <div class="col">
+      <Districts/>
+    </div>
+    <div class="col">
+      <Institutions/>
+    </div>
+    <div class="col">
+      <!-- Single option for test -->
+      <Years/>
+      <!-- Single option for test -->
+    </div>
+  </div>
+</template>
+
+<script>
+  import Districts from '@/components/filters/Districts';
+  import Institutions from '@/components/filters/Institutions';
+  import Years from '@/components/filters/Years';
+
+  export default {
+    name: "FiltersContainer",
+    components: {
+      Districts,
+      Institutions,
+      Years
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
