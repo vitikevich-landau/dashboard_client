f8b298175b5ba8dc0cb6f0629aa8aa5c0eaedd07
vitikevich <vitikevich@gmail.com>
2020-06-19 18:05:07 +0700

    # change opacity

--- src/configs.js
+++ src/configs.js
@@ -18,13 +18,13 @@ export const MONTHS = {
 
 export const COLORS = {
   chart: {
-    blue: 'rgb(54, 162, 235, 0.566)',
-    red: 'rgb(255, 99, 132, 0.566)',
-    purple: 'rgb(153, 102, 255, 0.566)',
-    orange: 'rgb(255, 159, 64, 0.566)',
-    yellow: 'rgb(255, 205, 86, 0.566)',
-    green: 'rgb(75, 192, 192, 0.566)',
-    grey: 'rgb(201, 203, 207, 0.566)'
+    blue: 'rgb(54, 162, 235, 0.666)',
+    red: 'rgb(255, 99, 132, 0.666)',
+    purple: 'rgb(153, 102, 255, 0.666)',
+    orange: 'rgb(255, 159, 64, 0.666)',
+    yellow: 'rgb(255, 205, 86, 0.666)',
+    green: 'rgb(75, 192, 192, 0.666)',
+    grey: 'rgb(201, 203, 207, 0.666)'
   },
   chartBorder: {
     blue: 'rgb(54, 162, 235)',
